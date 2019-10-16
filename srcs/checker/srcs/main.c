1bd8ea2f04dd95041ae667d14bfcaab3d1dad72f
msllav <mavui@student.42.fr>
2019-10-16 22:02:29 +0200

    cool

--- srcs/checker/srcs/main.c
+++ srcs/checker/srcs/main.c
@@ -8,9 +8,8 @@ int main(int ac, char **av)
 
 	for (int i = 0; i < ac; i++)
 		ft_printf("av[%d] = %s\n", i, av[i]);
-	while ((ret = get_next_line(0, &str)) > 0)
+	while ((ret = get_next_line(0, &str)) != 0)
 	{
-
 		if (str[0] == '\0' || str[0] == '\n')
 			break;
 		free(str);
