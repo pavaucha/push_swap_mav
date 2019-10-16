5749815d0383b771e19664a4ac8e98401f0e0537
msllav <mavui@student.42.fr>
2019-10-16 20:26:43 +0200

    Add GNL

--- srcs/checker/srcs/main.c
+++ srcs/checker/srcs/main.c
@@ -5,8 +5,12 @@ int main(int ac, char **av)
 	ac = 0;
 	int fd;
 	(void)av;
+	char *str;
 
 	fd = open(av[1], O_RDONLY);
-	ft_printf("fd = %d\n", fd);
+
+	while (get_next_line(fd, &str) != 0)
+		ft_printf("%s\n", str);
+
 	return 0;
 }
