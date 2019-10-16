a513dc0c88be350a97e2ee8485b77fc617bf4b60
msllav <mavui@student.42.fr>
2019-10-16 20:17:18 +0200

    init

--- srcs/checker/includes/checker.h
+++ srcs/checker/includes/checker.h
@@ -1,4 +1,8 @@
 #ifndef CHECKER_H
 # define CHECKER_H
 
+ #include <fcntl.h>
+ #include <unistd.h>
+ #include "ft_printf.h"
+
 #endif
--- srcs/checker/srcs/main.c
+++ srcs/checker/srcs/main.c
@@ -1,6 +1,12 @@
+#include "srcs/checker/includes/checker.h"
+
 int main(int ac, char **av)
 {
 	ac = 0;
+	int fd;
 	(void)av;
+
+	fd = open(av[1], O_RDONLY);
+	ft_printf("fd = %d\n", fd);
 	return 0;
 }
